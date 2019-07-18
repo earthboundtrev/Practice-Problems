df6289b4cc23c2e5c1ebb45f81a40aebd7c5f195
Trevor Thompson <trevthom18@gmail.com>
2019-07-17 23:18:35 -0400

    ReverseString practice assignment

---
+++ reverseString.js
@@ -0,0 +1,43 @@
+/*Given a string, reverse all the words which have odd length. The even length words are not changed.
+
+Examples
+
+
+Notes
+
+There is exactly one space between each word and no punctuation is used*/
+
+//need array.length variable
+//need to use the split function on the whitespace.
+//need to use a join function to create result
+
+// Think of join and split as yin and yang to remember how to deal with them
+
+let reverseOdd = (str) => {
+
+  let newStr = str.split(' ');
+  // let newArray = [];
+  //console.log(newStr);
+
+  let newArray = newStr.map(word => {
+
+    //return word;
+   if(word.length%2 === 0)
+   {
+      return word;
+   }
+   else 
+   {
+      return word.split('').reverse().join('');
+   }
+
+});
+ return newArray.join(' ');
+}
+
+console.log(reverseOdd("Bananas")); // "sananaB"
+
+console.log(reverseOdd("One two three four")); // "enO owt eerht four"
+
+console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
+//➞ "Make sure you only reverse words of odd length"
