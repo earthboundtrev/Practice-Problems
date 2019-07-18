3f180c865d0230a8edce21cf963de413c6961b8c
Trevor Thompson <trevthom18@gmail.com>
2019-07-18 19:57:14 -0400

    Refactored detectXo function to use more JavaScript like constructs. The solutions are now not correct, but it is a start

--- detect-XO.js
+++ detect-XO.js
@@ -6,40 +6,46 @@ Examples input/output:
 
 XO("ooxx") => true XO("xooxx") => false XO("ooxXm") => true XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true XO("zzoo") => false */
 
+let newXOArray = [];
 
-function XO(str) {
-    function XO(str) {
+let detectXO = (str) => {
+  let xCounter = 0;
+  let oCounter = 0;
 
-        let xCounter=0;
-        let oCounter=0;
-        
-          for(let i=0; i < str.length; i++)
-          {
-             if(str[i] === 'x' || str[i] === 'X')
-             {
-               xCounter=xCounter+1;
-             }
-             if(str[i] === 'o' || str[i] === 'O')
-             {
-               oCounter=oCounter+1;
-             }
-          }
-          
-          if(xCounter > oCounter)
-          {
-            return false;
-          }
-          
-          else if(xCounter < oCounter)
-          {
-            return false;
-          }
-          else
-          {
-             return true;
-          }
-            
+  newXOArray = str.split(" ");
+
+  newXOArray.forEach = (XOelement => {
+
+     if(XOelement === 'x' || XOelement === 'x')
+     {
+        xCounter++;  
+     }
+    else if(XOelement === 'o' || XOelement === '0')
+    {
+        oCounter++;
     }
+    else 
+    {
+       console.log('not x or o.');
+    }
+
+  });
+
+  if(xCounter > oCounter)
+  {
+    console.log(xCounter);
+  }
+  else if(oCounter > xCounter)
+  {
+    console.log(oCounter);
+  }
+  else{
+    console.log(xCounter);
+  }
+
+}
+
+detectXO('x0oxoxox');
 
 // This solution will solve the problem, but it solves the problem in a way that doesn't suit the language
 // This feels like a very C/C++ way to solve this problem does anyone have any tips for refactoring this?
