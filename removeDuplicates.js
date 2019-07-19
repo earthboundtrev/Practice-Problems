d8e383def05a00a8aecedb6ebbb4128fee85b0c7
Trevor Thompson <trevthom18@gmail.com>
2019-07-19 07:20:00 -0400

    Committing this work, so that I can access it from my other computer

---
+++ removeDuplicates.js
@@ -0,0 +1,34 @@
+/*Create a function that takes two arguments: an array and a number. In the array (the first argument), if an element occurs more than N times (the second argument), remove the extra occurrence(s) and return the result.
+
+Examples
+```deleteOccurrences([1, 1, 1, 1], 2) ➞ [1, 1]
+
+deleteOccurrences([13, true, 13, null], 1) ➞ [13, true, null]
+
+deleteOccurrences([true, true, true], 3) ➞ [true, true, true]``` */
+
+// number is the number of times the element occures in the array
+
+// The Javascript function .every() tests whether all elements in the array pass the test by the provider function
+// Every like forEach requires that you would use a callback function
+
+intCounter = 0;
+
+function deleteOccurrences(intArray, number) {
+
+    return intCounter++;
+
+}
+
+console.log()
+
+
+
+     //
+
+
+
+
+     //.every()
+
+deleteOccurrences([2,1,1,1]);
